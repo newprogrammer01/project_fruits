def74b39db91153519ff7332c74651499b29d63a
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2023-05-08 03:56:17 -0700

    name of the cheapest fruit

--- get_cheapest_fruit.py
+++ get_cheapest_fruit.py
@@ -7,5 +7,21 @@ def get_cheapest_fruit(data:str)->str:
     returns:
         name of the cheapest fruit
     """
-    # your code here
-    pass
+    row=data.split('\n')[1:]
+    price=[]
+    name=[]
+    max=0
+    for i in row:
+        price.append(float(i.split(',')[1]))
+    for i in row:
+        name.append(i.split(',')[0])
+    for i in price:
+        if i>max:
+            max=i
+    
+
+    return name[price.index(max)]
+data=open('fruits.csv').read()
+print(get_cheapest_fruit(data))
+   
+   
